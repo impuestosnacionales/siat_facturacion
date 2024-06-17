a13677459fa5f63e44882a31df2e77d7d3b1de7f
lauragzs <lauragonzalesc002@gmail.com>
2024-06-16 23:06:30 -0400

    l

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>ZZZZZ</title>
+  <title>CodingCreeper</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
